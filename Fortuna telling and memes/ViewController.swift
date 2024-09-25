904b3e2aec7f3272a2b7c63d3a3631cc191d8a3a
katrina-o <ekaterinaorlova25041994@gmail.com>
2024-09-25 12:48:23 +0300

    Initial Commit

---
+++ Fortuna telling and memes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fortuna telling and memes
+//
+//  Created by Катя on 25.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
