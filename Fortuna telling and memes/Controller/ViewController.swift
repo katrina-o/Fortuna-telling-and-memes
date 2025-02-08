64fff557df16f7d71aa2d3d91445ee6d3216c032
katrina-o <ekaterinaorlova25041994@gmail.com>
2025-02-08 14:29:02 +0300

    add animation

--- Fortuna telling and memes/Controller/ViewController.swift
+++ Fortuna telling and memes/Controller/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController, UISearchBarDelegate {
         let searchBar = UISearchBar()
         searchBar.searchBarStyle = .prominent
         searchBar.placeholder = "inter you question"
-        searchBar.barTintColor = .systemCyan
+        searchBar.barTintColor = .lightText
         
         return searchBar
     }()
@@ -27,7 +27,7 @@ class ViewController: UIViewController, UISearchBarDelegate {
         let button = UIButton()
         button.setTitle("Get prediction", for: .normal)
         button.setTitleColor(.darkGray, for: .normal)
-        button.backgroundColor = .clear
+        button.backgroundColor = .systemPink.withAlphaComponent(0.5)
         button.layer.cornerRadius = 10
         button.layer.masksToBounds = true
         button.addTarget(self, action: #selector(backPredictionButton), for: .touchUpInside)
@@ -38,15 +38,18 @@ class ViewController: UIViewController, UISearchBarDelegate {
     let memImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "catPre")
-        image.contentMode = .scaleAspectFit
-        
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 12
+//        image.setImage(image.image, animated: true)
         return image
     }()
     let yesButton: UIButton = {
         let button = UIButton()
         button.setTitle("👍🏼", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 35)
-        button.tag = 2
+        button.backgroundColor = .systemPink.withAlphaComponent(0.5)
+        button.layer.cornerRadius = 10
         button.addTarget(self, action: #selector(getPredictionButton), for: .touchUpInside)
         
         return button
@@ -56,6 +59,8 @@ class ViewController: UIViewController, UISearchBarDelegate {
         let button = UIButton()
         button.setTitle("👎🏼", for: .normal) 
         button.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+        button.backgroundColor = .systemYellow.withAlphaComponent(0.5)
+        button.layer.cornerRadius = 10
         button.addTarget(self, action: #selector(backPredictionButton), for: .touchUpInside)
         
         return button
@@ -66,21 +71,28 @@ class ViewController: UIViewController, UISearchBarDelegate {
         MemManager.getMemes { response in
             self.memes = response
         }
+        title = "Fortuna Telling and Memes"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "bookmark"), style: .plain, target: self, action: #selector(likeAndSave))
         initialize()
     }
   
-    
-    @objc func getPredictionButton( sender: UIButton) -> Bool {
-        if yesButton.currentTitle == sender.currentTitle {
-            return true
-        } else {
-            return false
-        }
-        getMemImages()
+    @objc func likeAndSave( sender: UIButton) {
+        
+    }
+    @objc func getPredictionButton( sender: UIButton) {
+        memImage.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            
+            UIView.animate(withDuration: 1.35, delay: 0,
+                                       usingSpringWithDamping: 0.25,
+                                       initialSpringVelocity: 5,
+                                       options: .curveEaseOut,
+                                       animations: {
+                                        
+                self.memImage.transform = .identity
+            })
     }
     
     @objc func backPredictionButton( sender: UIButton) {
-        let userAnswer = sender.currentTitle!
         getMemImages()
         nextMem()
     }
@@ -104,7 +116,7 @@ class ViewController: UIViewController, UISearchBarDelegate {
     }
     
     func initialize() {
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .systemBackground
         view.addSubview(memSearchBar)
         view.addSubview(memSearchButton)
         view.addSubview(memImage)
@@ -115,30 +127,39 @@ class ViewController: UIViewController, UISearchBarDelegate {
             make.top.equalToSuperview().inset(100)
             make.leading.trailing.equalToSuperview().inset(15)
         }
-        
         memSearchButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalToSuperview().inset(180)
+            make.width.equalTo(200)
         }
         
         memImage.snp.makeConstraints { make in
             make.center.equalToSuperview()
             make.height.equalTo(300)
-            make.width.equalTo(200)
+            make.width.equalTo(300)
         }
         
         yesButton.snp.makeConstraints { make in
             make.height.equalTo(100)
             make.width.equalTo(100)
             make.bottom.equalToSuperview().inset(120)
-            make.leading.equalToSuperview().inset(100)
+            make.leading.equalToSuperview().inset(90)
         }
         
         noButton.snp.makeConstraints { make in
-            make.height.equalTo(80)
-            make.width.equalTo(80)
+            make.height.equalTo(100)
+            make.width.equalTo(100)
             make.bottom.equalToSuperview().inset(120)
-            make.trailing.equalToSuperview().inset(100)
+            make.trailing.equalToSuperview().inset(90)
         }
     }
 }
+
+//extension UIImageView{
+//    func setImage(_ image: UIImage?, animated: Bool = true) {
+//        let duration = animated ? 0.3 : 0.0
+//        UIView.transition(with: self, duration: duration, options: .transitionCrossDissolve, animations: {
+//            self.image = image
+//        }, completion: nil)
+//    }
+//}
